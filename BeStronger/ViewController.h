8f951a16f1ed6a0dc36a66039b5a7794d27a76de
royzhuo <731924600@qq.com>
2016-11-24 23:19:22 +0800

    1.网络请求的单元测试

--- BeStronger/ViewController.h
+++ BeStronger/ViewController.h
@@ -10,6 +10,15 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *idTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *birthdayTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *createTimeTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *departTextField;
 
 @end
 
