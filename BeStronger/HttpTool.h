80c9c4c8d1b62dfcc6c4c42b0fb1215f8dd51c1a
royzhuo <731924600@qq.com>
2016-11-30 11:46:32 +0800

    1.新增afnetworking网络库 2.Podfile Podfile.lock文件

---
+++ BeStronger/HttpTool.h
@@ -0,0 +1,13 @@
+//
+//  HttpTool.h
+//  BeStronger
+//
+//  Created by Roy on 16/11/30.
+//  Copyright © 2016年 Roy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpTool : NSObject
+
+@end
---
+++ BeStronger/HttpTool.m
@@ -0,0 +1,21 @@
+//
+//  HttpTool.m
+//  BeStronger
+//
+//  Created by Roy on 16/11/30.
+//  Copyright © 2016年 Roy. All rights reserved.
+//
+
+#import "HttpTool.h"
+#import "AFNetworking.h"
+
+@implementation HttpTool
+
+
+
+
+
+
+
+
+@end
